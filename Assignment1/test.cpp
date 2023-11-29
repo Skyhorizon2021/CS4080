984a2dc23a340914a51298d10951af8a3f2012e7
Loc N <101013858+Skyhorizon2021@users.noreply.github.com>
2023-11-28 21:37:33 -0800

    test cpp

---
+++ Assignment1/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Matrix 
+{
+public:
+    int row;
+    int col;
+};
+
+
+
+int main()
+{
+    Matrix m1;
+    m1.col = 10;
+    m1.row = 10;
+}
